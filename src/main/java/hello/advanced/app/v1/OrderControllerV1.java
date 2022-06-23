a283472dfbf0e1f484e4f83e54d63ef91cb34f87
DavidJack-dev <[youngsik3200@gmail.com]>
2022-06-23 16:30:00 +0900

    로그 추적기 V2 - 파라미터로 동기화 개발

--- src/main/java/hello/advanced/app/v1/OrderControllerV1.java
+++ src/main/java/hello/advanced/app/v1/OrderControllerV1.java
@@ -1,5 +1,7 @@
 package hello.advanced.app.v1;
 
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV1;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,11 +12,19 @@ public class OrderControllerV1 {
 
 
     private final OrderServiceV1 orderServiceV0;
+    private final HelloTraceV1 trace;
 
     @GetMapping("/v1/request")
     public String request(String itemId) {
-        orderServiceV0.orderItem(itemId);
-
-        return "ok";
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderController.request()");
+            orderServiceV0.orderItem(itemId);
+            trace.end(status);
+            return "ok";
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e; // 예외를 꼭 던져줘야 한다. 그러지 않으면 이 예외를 먹어버린다.
+        }
     }
 }
--- src/main/java/hello/advanced/app/v1/OrderRepositoryV1.java
+++ src/main/java/hello/advanced/app/v1/OrderRepositoryV1.java
@@ -1,5 +1,7 @@
 package hello.advanced.app.v1;
 
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV1;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -7,14 +9,26 @@ import org.springframework.stereotype.Repository;
 @RequiredArgsConstructor
 public class OrderRepositoryV1 {
 
+    private final HelloTraceV1 trace;
+
 
     public void save(String itemId){
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderRepositoryV1.save()");
+            //저장 로직
+            if (itemId.equals("ex")){
+                throw new IllegalStateException(" 예외 발생! ");
+            }
+            sleep(1000);
 
-        //저장 로직
-        if (itemId.equals("ex")){
-            throw new IllegalStateException(" 예외 발생! ");
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e; // 예외를 꼭 던져줘야 한다. 그러지 않으면 이 예외를 먹어버린다.
         }
-        sleep(1000);
+
+
     }
 
     public void sleep(int millis){
